2e19474dbb91bc9530e9ec247ad6a3727f021427
liutastic <githubliutastic@163.com>
2021-01-23 16:12:49 +0800

    统一格式添加时间戳

---
+++ middlewares/responseFormatter.js
@@ -0,0 +1,27 @@
+const formatDate = require('../utils/dateFormat')
+
+let responseFormatter = async (ctx, next) => {
+  //先去执行路由
+  await next();
+
+  //如果有返回数据，将返回数据添加到data中
+  if (ctx.body) {
+    // console.log('ctx', ctx)
+    ctx.status = ctx.response.status
+    ctx.body = {
+      stamp: formatDate(new Date(), true),
+      code: ctx.response.status,
+      message: ctx.response.message,
+      data: ctx.body
+    }
+  } else {
+    ctx.status = ctx.response.status
+    ctx.body = {
+      stamp: formatDate(new Date(), true),
+      code: ctx.response.status,
+      message: ctx.response.message
+    }
+  }
+}
+
+module.exports = responseFormatter;
