f88763b9f1dd82e042bc2b4550f635b295e8c11e
liutastic <githubliutastic@163.com>
2021-01-25 15:56:21 +0800

    新增血常规检验模型

--- models/CBCTest.js
+++ models/CBCTest.js
@@ -3,8 +3,38 @@ const formatDate = require('../utils/dateFormat').formatDate
 
 const Schema = mongoose.Schema
 mongoose.set('useFindAndModify', false)
-const data = format(formatDate(new Date(), true))
+const date = format(formatDate(new Date(), true))
 
 const CBCSchema = new Schema({
-  
+  itemId: {
+    type: String,
+    required: false
+  },
+  itemName: {
+    type: String,
+    required: false
+  },
+  limit: {
+    type: Number
+  },
+  ceil: {
+    type: Number
+  },
+  unit: {
+    type: String
+  },
+  unitType: {
+    type: String
+  },
+  isDeleted: {
+    type: Number,
+    select: false,
+    default: 0
+  },
+  createdAt: {
+    type: String,
+    default: date
+  }
 })
+
+module.exports = CBCTest = mongoose.model('CBCTests', CBCSchema)
